69bb86d6bf0d92cd5cd8d5519f850798701adb98
Gr1ngoire <gringoirelamusicart@gmail.com>
2021-03-23 23:58:37 +0200

    Starting coding

--- HW_Task2/ViewController.swift
+++ HW_Task2/ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sliderRed: UISlider!
+    @IBOutlet weak var sliderGreen: UISlider!
+    @IBOutlet weak var sliderBlue: UISlider!
+    
+    @IBOutlet weak var labelRed: UILabel!
+    @IBOutlet weak var labelGreen: UILabel!
+    @IBOutlet weak var labelBlue: UILabel!
+    
+    @IBOutlet weak var inputRed: UITextField!
+    @IBOutlet weak var inputGreen: UITextField!
+    @IBOutlet weak var inputBlue: UITextField!
+    
+    @IBOutlet weak var resultView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        resultView.layer.cornerRadius = 
     }
 
 
